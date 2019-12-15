ab533a7184bf171637077ea1f59df24011630a88
Paul Luu <paul_luu.work@hotmail.com>
2019-12-15 11:08:37 -0500

    Fix travis configuration to test latest node

--- src/components/External.js
+++ src/components/External.js
@@ -6,7 +6,39 @@ import React, {Component} from 'react';
 class External extends Component {
   render() {
     return (
-        <p>External Projects</p>
+        <h1>External Projects</h1>
+        <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <h2>Rush (Incomplete) 
+                <span className="logo"><i className="fas fa-gamepad">
+                    </i>
+                </span>
+            </h2>
+            <div className="thumbnail">
+              <div className="caption" align="center">
+               <img src="../../public/images/hardtime.png" alt=""/>
+                <a className="btn-primary" href="/rush">Click here to play</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-6">
+            <h2>Hard Time 
+                <span className="logo"><i className="fas fa-gamepad">
+                    </i>
+                </span>
+            </h2>
+            <div className="thumbnail">
+              <div className="caption" align="center">
+                <img src="../../public/images/rush.png" alt=""/>
+                <a className="btn-primary" href="/hardtime">Click here to play</a>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
     );
   }
 }
