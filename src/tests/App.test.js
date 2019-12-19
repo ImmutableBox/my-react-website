77efe383a53b2d644095028580722435cd98639e
Paul Luu <paul_luu.work@hotmail.com>
2019-12-19 17:59:57 -0500

    Reconfig project

--- src/tests/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from '../components/App';
-
-test('renders copyright text', () => {
-  const { getByText } = render(<App />);
-  const copyrightElement = getByText('© 2019 Paul Luu');
-  expect(copyrightElement).toBeInTheDocument();
-});
