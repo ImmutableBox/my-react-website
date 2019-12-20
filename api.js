e2d3626ae9d0f89f26a0eca69849d3f7d4c23a89
Paul Luu <paul_luu.work@hotmail.com>
2019-12-19 21:24:45 -0500

    Clean README.md file

--- api.js
+++ api.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 // An api endpoint that returns a short list of items
 app.get('/api/getBlogFeed', (req, res) => {
@@ -26,3 +26,4 @@ app.get('*', (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port);
+console.log(__dirname, 'client/build');
