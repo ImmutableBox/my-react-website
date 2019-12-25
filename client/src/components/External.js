5bc5bc706f1b3ca1079e1f35816c216f49f8e63f
Paul Luu <paul_luu.work@hotmail.com>
2019-12-25 11:02:03 -0500

    Spliting game development sections

--- client/src/components/External.js
+++ client/src/components/External.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { FaGamepad } from 'react-icons/fa';
+import { FaGamepad, FaAdobe } from 'react-icons/fa';
+import { DiUnitySmall } from 'react-icons/di';
 
 /**
  * External project component.
@@ -29,71 +30,84 @@ class External extends Component {
               </div>
             </div>
           </div>
-          <div className="row">
-            <div className="p-card">
+          <div className="p-strip is-deep">
+            <div className="row">
               <h2>
-                Friendly Encounter - Created for my Project Management Course
+                <DiUnitySmall />
+                &nbsp;Games developed in Unity:
               </h2>
-              <iframe title="Friendly Encounter" src="/projects/FE-Web/index.html" name="FE" width="1024px" height="768px" />
-              <div className="col-sm-12">
-                <div className="p-card">
-                  Source code:
-                  <a href="https://github.com/ThomasNolte/PRJ666-Friendly-Encounters">
-                    https://github.com/ThomasNolte/PRJ666-Friendly-Encounters
-                  </a>
-                </div>
-              </div>
-            </div>
-            <hr />
-            <div className="p-card">
-              <h2>
-                  Hard Time
-              </h2>
-              <object width="600" height="450" data="/projects/HardEscape.swf"> HardEscape </object>
               <div className="p-card">
-                  Try to escape from jail!
-                <br />
-                  Controls: Up or W
-                <br />
-                  Left or A
-                <br />
-                  Down or S
-                <br />
-                  Right or D
-                <br />
-                  Spacebar - Interact with objects/Open doors
-                <br />
-                  Escape or P- Pauses the game
-                <br />
-                  R - Reset the level
+                <h2>
+                  Friendly Encounter - Created for my Project Management Course
+                </h2>
+                <iframe title="Friendly Encounter" src="/projects/FE-Web/index.html" name="FE" width="1024px" height="768px" />
+                <div className="col-sm-12">
+                  <div className="p-card">
+                    Source code:
+                    <a href="https://github.com/ThomasNolte/PRJ666-Friendly-Encounters">
+                      https://github.com/ThomasNolte/PRJ666-Friendly-Encounters
+                    </a>
+                  </div>
+                </div>
               </div>
+              <hr />
+              <div className="p-card">
+                <h2>
+                  Rush (Incomplete)
+                </h2>
+                <iframe title="Rush" src="/projects/Rush/index.html" name="Rush" width="1024px" height="768px" />
+                <div className="col-sm-12">
+                  <div className="p-card">
+                    Controls: Up or W
+                    <br />
+                    Left or A
+                    <br />
+                    Down or S
+                    <br />
+                    Right or D
+                    <br />
+                    Click to shoot
+                    <br />
+                    Source code:
+                    <a href="https://github.com/ImmutableBox/Mob-Swarm">
+                      https://github.com/ImmutableBox/Mob-Swarm
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <hr />
             </div>
-            <hr />
-            <div className="p-card">
+          </div>
+          <div className="p-strip is-deep">
+            <div className="row">
               <h2>
-                Rush (Incomplete)
+                <FaAdobe />
+                &nbsp;Games developed in Flash/ActionScript:
               </h2>
-              <iframe title="Rush" src="/projects/Rush/index.html" name="Rush" width="1024px" height="768px" />
-              <div className="col-sm-12">
+              <div className="p-card">
+                <h2>
+                    Hard Time
+                </h2>
+                <object width="600" height="450" data="/projects/HardEscape.swf"> HardEscape </object>
                 <div className="p-card">
-                  Controls: Up or W
+                    Try to escape from jail!
                   <br />
-                  Left or A
+                    Controls: Up or W
                   <br />
-                  Down or S
+                    Left or A
                   <br />
-                  Right or D
+                    Down or S
                   <br />
-                  Click to shoot
+                    Right or D
                   <br />
-                  Source code:
-                  <a href="https://github.com/ImmutableBox/Mob-Swarm">
-                    https://github.com/ImmutableBox/Mob-Swarm
-                  </a>
+                    Spacebar - Interact with objects/Open doors
+                  <br />
+                    Escape or P- Pauses the game
+                  <br />
+                    R - Reset the level
                 </div>
               </div>
             </div>
-            <hr />
           </div>
         </div>
       </div>
