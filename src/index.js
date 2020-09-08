89adcd578f615849d3076adcb61cd7c61465af0f
Paul Luu <paul_luu.work@hotmail.com>
2020-09-08 00:06:40 -0400

    Adding back

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './styles/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
