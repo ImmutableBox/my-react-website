2e4c3c522913606be4c2533ebd0c66f58573601d
Paul Luu <paul_luu.work@hotmail.com>
2020-09-17 10:59:46 -0400

    Change to nickname

--- src/components/sumo/SumoForm.js
+++ src/components/sumo/SumoForm.js
@@ -244,8 +244,7 @@ class SumoForm extends Component {
             <div className="row" style={{ background: '#FFF' }}>
               <form onSubmit={this.handleSubmit}>
                 <h2>
-                  Name
-                  (or enter an alias if you aren&apos;t comfortable with your real name):
+                  Name (or Nickname):
                 </h2>
                 <input type="text" onChange={(e) => this.setState({ name: e.target.value })} />
                 <hr />
