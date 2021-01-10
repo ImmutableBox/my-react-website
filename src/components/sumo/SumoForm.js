2c1a81cb029e9413ec3fa91dec18dc1601002bc8
Paul Luu <paul_luu.work@hotmail.com>
2021-01-10 16:22:34 -0500

    Close form

--- src/components/sumo/SumoForm.js
+++ src/components/sumo/SumoForm.js
@@ -18,7 +18,7 @@ class SumoForm extends Component {
       hoshitori: [],
       sumoLoading: false,
       gapiLoading: false,
-      closeForm: false,
+      closeForm: true,
       name: '',
       yokozunaOzeki: '',
       sekiwakeKomusubi: '',
